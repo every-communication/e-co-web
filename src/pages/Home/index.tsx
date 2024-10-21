f40782bd942e4370648ab1f27a4eeb090067a32b
rldnd <gi981226@gmail.com>
2024-10-22 01:45:19 +0900

    fix(friend): page styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,12 @@
-// import styles from "./homePage.module.scss";
-
 import TextPrimaryButton from "@/components/Common/Button/TextPrimaryButton";
 import { useMe } from "@/hooks";
+
+import styles from "./homePage.module.scss";
 
 const HomePage: React.FC = () => {
 	const { logout } = useMe();
 
-	return (
-		<>
-			home!
-			<TextPrimaryButton size="medium" type="button" onClick={logout}>
-				임시 로그아웃
-			</TextPrimaryButton>
-		</>
-	);
+	return <main className={styles.wrapper}></main>;
 };
 
 export default HomePage;
